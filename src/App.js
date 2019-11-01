eb9866ae3c576a6966f0b5d2a49dc050ea17c497
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-01 19:57:43 +0200

    Added css properties for searchBox and slider.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
+import Slider from './slider.js';
+import SearchBox from './searchBox.js';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>CO² Emissions</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
           Learn React
       </header>
+      <SearchBox />
+      <Slider />
     </div>
   );
 }
