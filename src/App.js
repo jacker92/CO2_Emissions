0d38bbcbdf46d1b4eccec7eb80ab013a31d921a2
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-03 09:14:10 +0200

    Update app layout and added callback-function to support data flow between components.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import React from 'react';
 import './App.css';
 import Slider from './slider.js';
 import SearchBox from './searchBox.js';
+import LineChart from './LineChart.js';
+import SortableComponent from './SortableComponent.js'
 
-function App() {
+let selectedCountriesData = [];
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>CO² Emissions</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-      <SearchBox />
-      <Slider />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    updateSortableList: {}
+  }
+
+  myCallback = (dataFromChild) => {
+    console.log("myCallBack!");
+    if(selectedCountriesData.indexOf(dataFromChild) === -1) {
+      selectedCountriesData.push(dataFromChild);
+    }
+    this.updateSortableList();
+  }
+
+    updateSortableList = () => {
+    this.setState({updateSortableList : !this.state.updateSortableList});
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>CO² Emissions</h1>
+          <p>
+            View CO² emissions in different countries
+          </p>
+        </header>
+        <SearchBox callbackFromParent={this.myCallback}/>
+        <div id="chartContainer">
+          <LineChart />
+          <div id="selectedList">
+            <h2>Selected Countries</h2>
+            <SortableComponent data={selectedCountriesData}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
