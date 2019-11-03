8d61bb8f362c0f5ef240f151ae22537a30d82868
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-03 09:15:34 +0200

    Hardcoded event listeners to list items for testing purposes.

--- src/searchBox.js
+++ src/searchBox.js
@@ -8,18 +8,33 @@ class SearchBox extends React.Component {
         this.state = {}
     }
 
-    componentDidMount()  {
-       var list =  document.getElementById('myUL');
-       list.style.display = 'none';
+    componentDidMount() {
+        console.log("Component did mount.");
+
+        let list = document.getElementById('myUL');
+        list.style.display = 'none';
+    }
+
+    whenClicked = (e) => {
+        console.log(e.target.innerText + " was clicked.");
+
+        let list = document.getElementById('myUL');
+        let li = list.getElementsByTagName('li');
+        for (let i = 0; i < li.length; i++) {
+            li[i].style.display = 'none';
+        }
+
+        this.props.callbackFromParent(e.target.innerText);
     }
 
     search = () => {
         // Declare variables
-        var input, filter, ul, li, a, i, txtValue, list;
+        let input, filter, ul, li, a, i, txtValue, list;
         input = document.getElementById('myInput');
         list = document.getElementById('myUL');
+        
         // If no text has been provided, do not show any results
-        if(input.value.length == 0) {
+        if (input.value.length == 0) {
             list.style.display = 'none';
         } else {
             list.style.display = "";
@@ -27,6 +42,7 @@ class SearchBox extends React.Component {
         filter = input.value.toUpperCase();
         ul = document.getElementById("myUL");
         li = ul.getElementsByTagName('li');
+
 
         // Loop through all list items, and hide those who don't match the search query
         for (i = 0; i < li.length; i++) {
@@ -45,15 +61,13 @@ class SearchBox extends React.Component {
             <div className="searchBoxContainer">
                 <input type="text" id="myInput" onKeyUp={this.search} placeholder="Type country name.." />
                 <ul id="myUL">
-                    <li><a href="#">Adele</a></li>
-                    <li><a href="#">Agnes</a></li>
-
-                    <li><a href="#">Billy</a></li>
-                    <li><a href="#">Bob</a></li>
-
-                    <li><a href="#">Calvin</a></li>
-                    <li><a href="#">Christina</a></li>
-                    <li><a href="#">Cindy</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Adele</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Agnes</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Billy</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Bob</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Calvin</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Christina</a></li>
+                    <li><a href="#" onClick={this.whenClicked}>Cindy</a></li>
                 </ul>
             </div>
         );
